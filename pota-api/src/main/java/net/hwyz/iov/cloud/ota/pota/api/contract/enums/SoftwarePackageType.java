9fbd1769d9b5398653d1d1e905b5f5e995ace950
hwyz_leo <hwyz.leo.ye@gmail.com>
2026-02-04 11:30:02 +0800

    改进: 简化软件包类型枚举定义

--- pota-api/src/main/java/net/hwyz/iov/cloud/ota/pota/api/contract/enums/SoftwarePackageType.java
+++ pota-api/src/main/java/net/hwyz/iov/cloud/ota/pota/api/contract/enums/SoftwarePackageType.java
@@ -2,8 +2,6 @@ package net.hwyz.iov.cloud.ota.pota.api.contract.enums;
 
 import lombok.AllArgsConstructor;
 
-import java.util.Arrays;
-
 /**
  * 软件包类型枚举类
  *
@@ -12,23 +10,12 @@ import java.util.Arrays;
 @AllArgsConstructor
 public enum SoftwarePackageType {
 
-    FULL("全量", 1),
-    DELTA("差分", 2);
+    FULL("全量"),
+    DELTA("差分");
 
     /**
      * 名称
      */
     public final String label;
-    /**
-     * 值
-     */
-    public final int value;
-
-    public static SoftwarePackageType valOf(Integer val) {
-        return Arrays.stream(SoftwarePackageType.values())
-                .filter(softwarePackageType -> softwarePackageType.value == val)
-                .findFirst()
-                .orElse(null);
-    }
 
 }
